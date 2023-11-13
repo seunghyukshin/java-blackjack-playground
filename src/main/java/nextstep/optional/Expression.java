84994a06d871f2368779c23b14d60fc055d84983
seunghyukshin <auseunghyuk@gmail.com>
2023-11-13 23:32:31 +0900

    Optional 실습3

--- src/main/java/nextstep/optional/Expression.java
+++ src/main/java/nextstep/optional/Expression.java
@@ -1,5 +1,11 @@
 package nextstep.optional;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 enum Expression {
     PLUS("+"), MINUS("-"), TIMES("*"), DIVIDE("/");
 
@@ -9,17 +15,24 @@ enum Expression {
         this.expression = expression;
     }
 
-    private static boolean matchExpression(Expression e, String expression) {
-        return expression.equals(e.expression);
+    private static boolean matchExpression(Expression expression, String expressionString) {
+        return expressionString.equals(expression.expression);
     }
 
-    static Expression of(String expression) {
-        for (Expression v : values()) {
-            if (matchExpression(v, expression)) {
-                return v;
-            }
-        }
+    static Expression of(String expressionString) {
 
-        throw new IllegalArgumentException(String.format("%s는 사칙연산에 해당하지 않는 표현식입니다.", expression));
+        List<Optional<Expression>> optionalList = Arrays.stream(values())
+                .map(Optional::ofNullable)
+                .collect(Collectors.toList());
+
+        List<Expression> expressionList = optionalList.stream()
+                .filter(Optional::isPresent)
+                .map(Optional::get)
+                .collect(Collectors.toList());
+
+        return expressionList.stream()
+                .filter(expression -> matchExpression(expression, expressionString))
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException(String.format("%s는 사칙연산에 해당하지 않는 표현식입니다.", expressionString)));
     }
 }
