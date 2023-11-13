c7b6754af22fe283e2d1ac6dd6e0cdf2ccb134b7
seunghyukshin <auseunghyuk@gmail.com>
2023-11-13 22:55:58 +0900

    Optional 실습2

--- src/main/java/nextstep/optional/Users.java
+++ src/main/java/nextstep/optional/Users.java
@@ -3,6 +3,7 @@ package nextstep.optional;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class Users {
@@ -12,20 +13,25 @@ public class Users {
             new User("crong", 35),
             new User("pobi", 30),
             new User("jk", 40),
-            new User("honux", 45));
+            new User("honux", 45),
+            null
+    );
 
     User getUser(String name) {
-        Optional<User> userOptional = users.stream()
+
+        List<Optional<User>> optionalList = users.stream()
+                .map(user -> Optional.ofNullable(user))
+                .collect(Collectors.toList());
+
+        List<User> userList = optionalList.stream()
+                .filter(Optional::isPresent)
+                .map(Optional::get)
+                .collect(Collectors.toList());
+
+
+        return userList.stream()
                 .filter(user -> user.matchName(name))
-                .findFirst();
-
-        return userOptional.orElse(DEFAULT_USER);
-
-//        for (User user : users) {
-//            if (user.matchName(name)) {
-//                return user;
-//            }
-//        }
-//        return DEFAULT_USER;
+                .findFirst()
+                .orElse(DEFAULT_USER);
     }
 }
